d1a96e34d3f892f0b6ac42252937c5083a1018ab
Jonas Bork <jonas10bork@gmail.com>
2025-09-23 15:53:14 +0200

    Used Mock instead of types for mocking in tests

--- tests/test_health.py
+++ tests/test_health.py
@@ -1,4 +1,4 @@
-import types
+from unittest.mock import Mock
 
 
 def test_health_endpoint(client):
@@ -11,21 +11,22 @@ def test_health_endpoint(client):
 
 def test_ready_endpoint(client, monkeypatch):
     """Test the ready endpoint"""
-    assume_keycloak_ready_state(monkeypatch, True)
+    fake_response = assume_keycloak_ready_state(monkeypatch, True)
 
     response = client.get("/readyz")
+    fake_response.json.assert_called_once()
     assert response.status_code == 200
     data = response.get_json()
     assert data["status"] == "ready"
 
 
 def assume_keycloak_ready_state(monkeypatch, is_ready: bool):
-    def fake_get_keycloak_ready_response():
-        return types.SimpleNamespace(
-            json=lambda: {"status": "UP" if is_ready else "DOWN"}
-        )
+    fake_response = Mock()
+    fake_response.json.return_value = {"status": "UP" if is_ready else "DOWN"}
 
     monkeypatch.setattr(
         "src.blueprints.health.try_get_keycloak_ready_response",
-        fake_get_keycloak_ready_response,
+        lambda: fake_response,
     )
+
+    return fake_response
