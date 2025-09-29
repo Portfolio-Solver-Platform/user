e899508f959de12c60b1b91e384bdb636cef51c3
Jonas Bork <jonas10bork@gmail.com>
2025-09-29 14:57:58 +0200

    Improved login test

--- tests/api/test_login.py
+++ tests/api/test_login.py
@@ -9,6 +9,10 @@ def test_login(client):
         api_url("/login"), json={"username": "user", "password": "user"}
     )
     assert response.status_code == 200
-    print(response.json())
-    assert False
-    pass
+    data = response.json()
+
+    assert "access_token" in data
+    assert "refresh_token" in data
+    assert "expires_in" in data
+    assert "refresh_expires_in" in data
+    assert "token_type" in data
